fdc61b18e644ff61d09ce1259e74b83cfa14b2eb
Micah Fitzgerald <mcfitz2@gmail.com>
2018-10-17 19:09:50 +0000

    first

---
+++ broker/index.js
@@ -0,0 +1,38 @@
+var mosca = require("mosca");
+//var ascoltatore = {
+//    type: 'redis',
+//    redis: require('redis'),
+//    db: 12,
+//    port: 6379,
+//    return_buffers: true, // to handle binary payloads
+//    host: "redis"
+//};
+
+
+var settings = {
+    port: 8888,
+//    backend: ascoltatore,
+    logger: {
+        name: "broker",
+        level: 30,
+    }
+};
+
+var server = new mosca.Server(settings);
+
+server.on('ready', setup);
+server.on('clientDisconnected', function(client) {
+    server.publish("feeders/"+client.id+"/disconnect", "OK");
+});
+
+function setup() {
+        server.authorizePublish = (client, topic, payload, callback) => {
+            var auth = client.id == topic.split('/')[1] || client.id.indexOf("debug") == 0;
+            callback(null, auth);
+        }
+        server.authorizeSubscribe = (client, topic, callback) => {
+            var auth = client.id == topic.split('/')[1] || client.id.indexOf("tester") == 0;
+            callback(null, auth);
+        }
+    console.log('Mosca server is up and running');
+}
